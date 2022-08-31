9cde48f118effe920cdb302b60ba41fee12003e4
Jon <25947147+JonrGull@users.noreply.github.com>
2022-08-31 18:14:02 +0900

    refactor/fix: refactor sass grouping, hamburger menu populates

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar__links")[0];
+const navbarBtn = document.getElementsByClassName("navbar__btn-container")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+  navbarBtn.classList.toggle("active");
 });
